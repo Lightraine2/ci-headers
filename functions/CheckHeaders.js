060cd7d6071dc40620f18a211e8214f7ab10e77a
Lightraine2 <laurencedale1986@gmail.com>
2022-01-08 12:19:17 +0000

    ok up to error handling then unit tests

--- functions/CheckHeaders.js
+++ functions/CheckHeaders.js
@@ -1,22 +1,40 @@
 const checkMyHeaders = require('check-my-headers')
 
 module.exports = function checkHeaders(url) {
+    
     console.log('\n')
     console.log('Checking Security Headers returned from ' + url)
     console.log('\n')
+
+    try {
     checkMyHeaders(url)
-.then(({ messages, headers, status }) => {
+    .then(({ messages, headers, status }) => {
     if (status != 200) {
         console.log('HTTP Error returned - check url formatting including http/https protocol i.e. "https://site.address.com"')
-    } else {
-        console.log('Successful connection established')
-    }
+        }
+    //TODO DNS failure handling   
+    // if (status = 500 | 503 | 504) {
+    //     console.log("Application was found but Web server returned errors: ")
+    //     console.log("HTTP Status code: " + status)
+    //     return
+    // }
+    
+    else {
+    console.log('Successful connection established')
     console.log(`Status code: ${status}`)
     console.log('\n')
     console.log(`Messages:`)
     console.log(messages)
     console.log("Current headers:")
     console.log(headers)
-})
+    } 
+
+    })
+
+    } catch (error) {
+        console.log("Something went wrong")
+        //console.error(error)
+    }
+    
 }
 
