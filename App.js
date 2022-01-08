60e0daee687c0cd2f89a1bf330f10742ca5c234d
Lightraine2 <laurencedale1986@gmail.com>
2022-01-08 11:33:19 +0000

    added readme

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ const checkHeaders = require('./functions/CheckHeaders')
 const juiceShopUrl = "https://juice-shop.herokuapp.com/#/"
 const userUrl = process.argv[2]
 
+const config = require('./ci-config.json')
+
 //Add check to make sure app has been run properly i.e. only one argument and it's correctly formatted url with http/https protocol specified
 
-//console.log(userUrl)
- 
-checkHeaders(juiceShopUrl)
+if (userUrl) {
+    checkHeaders(userUrl)
+} else {
+    checkHeaders(config['app-url'])
+}
 
 
-
+//TODO - add error handling for DNS failure
+//TODO - add error handling for no internet
+//TODO - add return formatting for highlighting security issues
--- functions/CheckHeaders.js
+++ functions/CheckHeaders.js
@@ -1,9 +1,18 @@
 const checkMyHeaders = require('check-my-headers')
 
 module.exports = function checkHeaders(url) {
+    console.log('\n')
+    console.log('Checking Security Headers returned from ' + url)
+    console.log('\n')
     checkMyHeaders(url)
 .then(({ messages, headers, status }) => {
+    if (status != 200) {
+        console.log('HTTP Error returned - check url formatting including http/https protocol i.e. "https://site.address.com"')
+    } else {
+        console.log('Successful connection established')
+    }
     console.log(`Status code: ${status}`)
+    console.log('\n')
     console.log(`Messages:`)
     console.log(messages)
     console.log("Current headers:")
